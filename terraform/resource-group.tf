bd6b44fc49247e49d83055be4c23876c2b3f8754
divyang2105 <divyang.7@outlook.com>
2023-01-13 12:18:22 +0000

    add tag to resource group

--- terraform/resource-group.tf
+++ terraform/resource-group.tf
@@ -1,4 +1,8 @@
 resource "azurerm_resource_group" "synapse_rg" {
   name = "${var.environment}-${var.resource_group_name}-rg"
   location = var.location
+
+  tags = {
+    "enviroment" = "dev"
+  }
 }
